6683f9be553a89fff9c8f40ea67b5e2e7ed7d126
Mark Granade <88593585+MarkGranade@users.noreply.github.com>
2022-01-05 13:07:31 -0500

    got indexedDb set up and working

---
+++ public/service-worker.js
@@ -0,0 +1,4 @@
+const APP_PREFIX = "BudgetTracker-";
+const VERSION = "version_01";
+const CACHE_NAME = APP_PREFIX + VERSION;
+const FILES_TO_CACHE = ["./css/styles.css", "./js/index.js", "./index.html"];
